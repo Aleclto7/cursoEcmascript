816120d69bfa90d4769e88470614c2b6bcb05433
Alexis Calixto <alexiscfx1@gmail.com>
2023-10-04 12:44:39 -0300

    Modulos

---
+++ src/es6/08-module.js
@@ -0,0 +1,5 @@
+// Modulos - Manera de importar y acceder a la logica de un archivo para usarla en nuestro codigo
+// Aqui sera donde traeremos el modulo
+import hello from "./module.js" // 'Import' permite importar la logica llamando al codigo por su nombre 'hello' y por su direccion
+
+hello();
---
+++ src/es6/module.js
@@ -0,0 +1,6 @@
+// Este sera el modulo
+const hello = () => {
+    console.log('Hello');
+}
+
+export default hello;   // Con 'Export' permitimos que se accesa a la funcion a travez del nombre 'hello'
