e8d0a1fa10ece67a1869fba75ca3e80830c0a17f
Alexis Calixto <alexiscfx1@gmail.com>
2023-10-07 12:35:33 -0300

    ECMA7 - Exponeciales y array.includes

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,2 @@
+const data = 3 ** 4;    // Nuevo operador para obtener una potencia entre dos valores
+console.log(data);
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,5 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numbers.includes(4));   // '.includes' nos permite saber si el array incluye un valor en especifico
+
+let names = ['Oscar', 'David', 'Ana']
+console.log(names.includes('oscar'));   // Tener en cuenta mayusculas en caso de strings porque toma como otro valir
