04fb9089ce4130837a68c83d0389127e925a2a83
Alexis Calixto <alexiscfx1@gmail.com>
2023-10-10 13:03:02 -0300

    Object entries y values

---
+++ src/es8/00-object-entries.js
@@ -0,0 +1,3 @@
+// ECMA8 Object.entries - Transforma cada par 'clave-valor' del objeto en arreglos, de manera estructurada y mas legible
+const countries = {MX: 'Mexico', CO: 'Colombia', CH: 'Chile', PE: 'Peru'};
+console.log(Object.entries(countries));
---
+++ src/es8/01-object-values.js
@@ -0,0 +1,3 @@
+// ECMA8 Object.entries - Tae de un objeto y transforma solo los valores en un arreglo (array).
+const countries = {MX: 'Mexico', CO: 'Colombia', CH: 'Chile', PE: 'Peru'};
+console.log(Object.values(countries));
