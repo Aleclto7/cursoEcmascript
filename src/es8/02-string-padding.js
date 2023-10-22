7e9767e2113337e6dca216dd0dd74fe0f94a527c
Alexis Calixto <alexiscfx1@gmail.com>
2023-10-22 12:19:38 -0300

    StringPadding and TrailingCommas

---
+++ src/es8/02-string-padding.js
@@ -0,0 +1,4 @@
+const string = 'Hello';
+
+console.log(string.padStart(7, '___')); // Inserta '___' al inicio hasta llegar a los carateres especificados '7'  
+console.log(string.padEnd(7, '___'));   // Inserta '___' al final hasta llegar a los carateres especificados '7' 
---
+++ src/es8/04-trailing-commas.js
@@ -0,0 +1,4 @@
+const arr = [2, 4, 7, 8, , , , , 10];   // ahora se puede crear items vacios a un array para posterior asignacion
+
+console.log(arr);
+console.log(arr.length);
