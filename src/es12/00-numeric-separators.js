b822baece6e67ed687694c4e6170be28c10f5946
Alexis Calixto <alexiscfx1@gmail.com>
2023-11-03 12:51:06 -0300

    Numeric Separators and Replace All

---
+++ src/es12/00-numeric-separators.js
@@ -0,0 +1,4 @@
+//ECMA12 - Numeric Separators - Ayuda visual en los casos donde tenemos valores numericos muy grandes.
+const value = 100_000_000_000_000;  // Con '_' los separamos para poder entenderlos mejor visualmente. 
+
+console.log(value); // No afectara a la hora de trabajar con ellos.
---
+++ src/es12/01-replaceall.js
@@ -0,0 +1,4 @@
+//ECMA12 - Replace All - Filtro para buscar y reemplazar textos
+const string = 'JavaScript es un maravilloso lenguaje de programacion'
+const replacedString = string.replace('JavaScript', 'TypeScript') // 'replace' busca y remplaza el primer valor por el segundo valor ingresado
+console.log(replacedString);    
